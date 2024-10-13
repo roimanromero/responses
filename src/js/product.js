b2ebade9befcddb7c692f5816437c52c7032269c
roimanromero <145807673+roimanromero@users.noreply.github.com>
2024-10-12 20:15:57 -0500

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,23 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+//i changed this function to instead append new item to cart rather than overwriting the items.
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //get current cart item from localStorage
+  let cartItems = getLocalStorage("so-cart");
+
+  //check if cartItem is an array
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  //add the new product to the cart array
+  cartItems.push(product);
+
+  //save the updated cart back to localStorage
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
